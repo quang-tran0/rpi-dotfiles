279dbd485a33550555a4fdb352de95dea9caa018
Quang Tran <quang.trd05@gmail.com>
2026-08-07 20:13:26 +0000

    Upgrade fastfetch config

--- home/dot_oh-my-zsh/custom/aliases.zsh
+++ home/dot_oh-my-zsh/custom/aliases.zsh
@@ -39,9 +39,9 @@ chezmoi() {
             else
                 shift # Bỏ tham số "push"
                 local msg="${1:-Update dotfiles}"
-                command chezmoi git add . && \
-                command chezmoi git -- commit -m "$msg" && \
-                command chezmoi git -- push -u origin main
+                command chezmoi git add .
+                command chezmoi git -- commit -m "$msg"
+                command chezmoi git -- push origin main
             fi
             ;;
         pull)
@@ -49,7 +49,7 @@ chezmoi() {
                 echo -e "\033[1;33m[WARNING] Chezmoi đã có lệnh 'pull' native! Đang chuyển sang lệnh hệ thống...\033[0m"
                 command chezmoi "$@"
             else
-                command chezmoi git -- pull -u origin main
+                command chezmoi git pull origin main
                 command chezmoi apply
             fi
             ;;
